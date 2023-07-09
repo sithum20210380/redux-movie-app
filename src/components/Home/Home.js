633f1246d3ffc751275f9ff6c499e7072b8b7f3d
Sithum Raveesha <sithum.20210380@iit.ac.lk>
2023-07-10 00:10:42 +0530

    updated the componets

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,9 +2,12 @@ import React, { useEffect } from 'react';
 import MovieListing from '../MovieListing/MovieListing';
 import {APIkey} from '../../common/apis/movieApiKey';
 import movieApi from '../../common/apis/movieApi';
+import { useDispatch } from 'react-redux';
+import { addMovies } from '../../features/movies/movieSlice';
 
 const Home = () => {
-    const movieText = 'harry'
+    const movieText = 'harry';
+    const dispatch = useDispatch();
     useEffect(() => {
         const fetchMovies = async () => {
             const response = await movieApi
@@ -13,6 +16,7 @@ const Home = () => {
                 console.log('Err: ', err);
             });
             console.log('The response from API ', response)
+            dispatch(addMovies)
         };
         fetchMovies();
     }, []);
--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { UseSelector, useSelector } from 'react-redux';
+import { getAllMovies } from '../../features/movies/movieSlice';
+import MovieCard from '../MovieCard/MovieCard';
 
 const MovieListing = () => {
-    return ( 
+    const movies = useSelector(getAllMovies);
+    let renderMovies = "";
+    //console.log(movies);
+    renderMovies = movies.Response === "True" ? (
+        movies.Search.map((movie, index) => {
+            <MovieCard key={index} data= {movie}/>
+        })
+    ) : (
+        <div className='movie-error'><h3>{movies.Error}</h3></div>
+    );
+    return 
         <div>
             MovieListing
         </div>
-     );
+     
 }
  
 export default MovieListing;
